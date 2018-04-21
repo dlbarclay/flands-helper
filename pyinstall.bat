dc56d7fca8453510ed80f59a6bc3f123b633d51a
David Barclay <dbarclay.inbox@gmail.com>
2018-04-21 16:00:52 +1200

    Fixed res path in pyinstall.bat

--- pyinstall.bat
+++ pyinstall.bat
@@ -1,6 +1,6 @@
 @echo off
 pyinstaller --noconfirm --log-level=WARN ^
     --onefile --noconsole --icon "res/tickbox.ico" ^
-    --add-data="res;." ^
+    --add-data="res;res" ^
     flandshelper.py
 
