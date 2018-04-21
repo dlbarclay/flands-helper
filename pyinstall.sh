d5d0bd973bd1c279b1b52abdda20cde36b508dc5
David Barclay <dbarclay.inbox@gmail.com>
2018-04-21 16:12:50 +1200

    pyinstall.sh: updated res path

--- pyinstall.sh
+++ pyinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pyinstaller --noconfirm --log-level=WARN \
     --onefile --noconsole --icon "res/tickbox.ico" \
-    --add-data="res:." \
+    --add-data="res:res" \
     flandshelper.py
 
