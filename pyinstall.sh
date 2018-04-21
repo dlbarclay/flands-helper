dd2088803fc9b232924836b64f268d042c682184
David Barclay <dbarclay.inbox@gmail.com>
2018-04-21 15:18:29 +1200

    pyinstall.sh: moved res to .

--- pyinstall.sh
+++ pyinstall.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 pyinstaller --noconfirm --log-level=WARN \
     --onefile --noconsole --icon "res/tickbox.ico" \
-    --add-data="res:res" \
-    --add-data="res:res" \
+    --add-data="res:." \
     flandshelper.py
 
